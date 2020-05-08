3d5095098f5ed677e7f71c631cdce4bad6cc466f
anactasi2607 <anactasi2607@yandex.ru>
2020-05-08 18:35:15 +0300

    Знакомство с хуками, начало разработки взаимодействия приложения с сервером git

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import {AlertContext} from '../context/alert/AlertContext';
 
 export const Search = () => {
+  const [value, setValue] = useState('');
+  const {show} = useContext(AlertContext);
+  const onSubmit = event => {
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+    if (value.trim()) {
+      console.log('Make request with: ', value);
+    } else {
+      show('Введите данные пользователя!');
+    }
+  }
+
   return (
     <div>
       <input 
         type="text"
-        className="form-control"
+        className="form-control mb-4"
         placeholder="Введите ник пользователя"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+        onKeyPress={onSubmit}
       />
     </div>
   )
